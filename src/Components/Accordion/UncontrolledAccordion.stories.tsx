b850961c217f5256506b000ad90906fd54441f45
IJTINK7 <ijtink7@mail.ru>
2023-09-27 09:28:12 +0300

    Creating story for "UncontrolledAccordion" components

---
+++ src/Components/Accordion/UncontrolledAccordion.stories.tsx
@@ -0,0 +1,10 @@
+import {UncontrolledAccordion} from './UncontrolledAccordion';
+import React from "react";
+
+export default {
+	component: UncontrolledAccordion,
+};
+
+export const FullAccordion =() => {
+	return <UncontrolledAccordion title={"Menu"}/>
+}
