e462a591c106b3f000449beb922266c026f1b22b
IJTINK7 <ijtink7@mail.ru>
2023-10-04 19:50:44 +0300

    Rendering changed data after changing(IMMUTABLE) initial state, using "React.memo" HOC (Good practice).

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -16,9 +16,23 @@ const Users = React.memo(BadUsers) // creating of container component
 export const Example = () =>{
 	const [counter, setCounter] = useState(0)
 	const [users, setUsers] = useState(["Roma", "Anna", "Bob"])
+	const AddUser = () => {
+		// 1 // If we use a mutable change of our initial state,
+		// it won't render our data.
+
+		// const newUsers = [...users]
+		// users.push("Anton" + new Date().getTime())
+		// setUsers(newUsers)
+
+		//2 That's why we have to use immutable changing of our initial state
+		// (for rendering new data after calling "AddUser" function)
+
+		setUsers([...users, "Anton" + new Date().getTime()])
+	}
 	return(
 		<div>
 			<button onClick={()=>setCounter(counter + 1)}>+</button>
+			<button onClick={AddUser}>Add user</button>
 			<NewMessagesCounter count={counter}/>
 			<Users users={users}/>
 		</div>
