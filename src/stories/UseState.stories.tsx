d217b7d9ae9cad48c7a288701163f196d05f3d2e
IJTINK7 <ijtink7@mail.ru>
2023-10-24 23:09:38 +0300

    Adding function as initial state for "useState" hook

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -3,8 +3,12 @@ import React, {useState} from "react";
 export default {
 	title: "useState demo"
 }
+const generateData = () => {
+	return 123
+}
 export const Example = () => {
-	const [counter, setCounter] = useState(0)
+	console.log("Example")
+	const [counter, setCounter] = useState(generateData)
 	return (
 		<div>
 			<button onClick={() => setCounter(counter + 1)}>+</button>
