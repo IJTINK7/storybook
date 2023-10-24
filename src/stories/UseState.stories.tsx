dccdb796521ea2e27f394f9dddfdd22d40ede8ec
IJTINK7 <ijtink7@mail.ru>
2023-10-24 23:29:18 +0300

    Finishing studying about advanced "useState" hook

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -1,3 +1,21 @@
+// import React, {useState} from "react";
+//
+// export default {
+// 	title: "useState demo"
+// }
+// const generateData = () => {
+// 	return 123
+// }
+// export const Example = () => {
+// 	console.log("Example")
+// 	const [counter, setCounter] = useState(generateData)
+// 	return (
+// 		<div>
+// 			<button onClick={() => setCounter(counter + 1)}>+</button>
+// 		</div>
+// 	)
+// }
+
 import React, {useState} from "react";
 
 export default {
@@ -5,7 +23,7 @@ export default {
 }
 const generateData = () => {
 	console.log("generateData")
-	return 123
+	return 456
 }
 export const Example = () => {
 	console.log("Example")
