72b5c37efd7b81e29b8f59874b261fed4041d5da
IJTINK7 <ijtink7@mail.ru>
2023-10-24 23:26:21 +0300

    Adding function as argument for "setCounter" function in "useState" hook

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -4,14 +4,19 @@ export default {
 	title: "useState demo"
 }
 const generateData = () => {
+	console.log("generateData")
 	return 123
 }
 export const Example = () => {
 	console.log("Example")
 	const [counter, setCounter] = useState(generateData)
+	const changer = (state: number) => {
+		return state + 1
+	}
 	return (
 		<div>
-			<button onClick={() => setCounter(counter + 1)}>+</button>
+			<button onClick={() => setCounter(changer)}>+</button>
+			{counter}
 		</div>
 	)
 }
