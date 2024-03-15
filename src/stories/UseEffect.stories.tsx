5af52b6dc6e650e1d5c35e997abbf2e8ba58e966
IJTINK7 <ijtink7@mail.ru>
2024-03-15 19:06:26 +0300

    Adding some useEffects with the rules of using

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -9,8 +9,14 @@ export const SimpleExample = () => {
 	const [counter, setCounter] = useState(0)
 	console.log("SimpleExample")
 	useEffect(()=>{
-		console.log("useEffect")
+		console.log("useEffect every render")
+	})
+	useEffect(()=>{
+		console.log("useEffect only first render(ComponentDidMount)")
 	}, [])
+	useEffect(()=>{
+		console.log("useEffect first render and every counter variable changing")
+	}, [counter])
 	const onclickHandler = () => {
 		setCounter(counter+1)
 	}
